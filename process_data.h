e6dcd10ff0028aede5c321bc60989415ce43e1d2
EthanPhonsouk <ethan.phonsouk@hotmail.com>
2025-03-06 21:37:46 -0800

    progress to processdata processCommands method

--- process_data.h
+++ process_data.h
@@ -1,18 +1,26 @@
 #ifndef PROCESS_DATA_H_
 #define PROCESS_DATA_H_
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "hash_table.h"
+#include "Transactions.h"
 
-#include <iostream> 
-#include <string> 
+using namespace std;
 
-class ProcessData{
+class ProcessData
+{
+    public:
+        ProcessData();
+        void processCommands();
 
+    private:
+        HashTable storedMovies;
+        HashTable storedCustomers;
 
-
-
-
-
+        void initializeMovieData(ifstream &stream);
+        void initializeCustomerData(ifstream &stream);
 };
-
 
 #endif
