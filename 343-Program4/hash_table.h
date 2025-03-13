7e0c9b2f112a18ae0c14e9e6b631e209bf22b8a4
EthanPhonsouk <ethan.phonsouk@hotmail.com>
2025-03-12 21:51:03 -0700

    merge changes from debug to main

--- 343-Program4/hash_table.h
+++ 343-Program4/hash_table.h
@@ -11,15 +11,15 @@ using namespace std;
 
 const int TABLE_SIZE = 101; // change to be a prime number greater than or equal to expected # of items
 
-template <typename T>
+template <class T>
 class HashTable
 {
     public:
         HashTable();
         ~HashTable();
 
-        bool insertString(const string value, const T* objectPtr);  // insert object using string to get key
-        bool insertInt(const int value, const T* objectPtr);     // insert object using int to get key
+        bool insertString(const string value, T* objectPtr);  // insert object using string to get key
+        bool insertInt(const int value, T* objectPtr);     // insert object using int to get key
         void display() const; // Print all stored movies 
 
     private:
@@ -29,5 +29,127 @@ class HashTable
         int getKeyFromString(const string value, const int steps) const;
         int getKeyFromInt(const int value, const int steps) const;
 };
+
+template <typename T>
+HashTable<T>::HashTable() {
+    for (int i = 0; i < TABLE_SIZE; i++) // sets pointers to nullptr (empty array)
+    {
+        table[i] = nullptr;
+    }
+}
+
+template <typename T>
+bool HashTable<T>::insertString(const string value,  T* newObjectPtr)
+{
+    if (newObjectPtr == nullptr)
+    {
+        return false;
+    }
+
+    int steps = 0;
+    while (steps < TABLE_SIZE) // traverse until every spot has been visited
+    {
+        int index = getKeyFromString(value, steps);
+        if (table[index] == nullptr) // empty index
+        {
+            table[index] = newObjectPtr;
+            return true;
+        }
+        steps++;
+    }
+    return false;
+}
+
+template <typename T>
+bool HashTable<T>::insertInt(const int value, T* newObjectPtr)
+{
+    if (newObjectPtr == nullptr)
+    {
+        return false;
+    }
+
+    int steps = 0;
+    while (steps < TABLE_SIZE) // traverse until every spot has been visited
+    {
+        int index = getKeyFromInt(value, steps);
+        if (table[index] == nullptr) // empty index
+        {
+            table[index] = newObjectPtr;
+            return true;
+        }
+        steps++;
+    }
+    return false;
+}
+
+// h(x1) = (x1 % 53)
+    // x1 = first + second
+// h_2(x2) = (37 - (x2 % 37))
+    // x2 = first * second
+
+// D(i) = h_2(x2) * i, where i is initially = 0
+
+// h(x1) + D(i) % TABLE_SIZE
+// (((first + second) % 53) + [(37 - (first * second % 37)) * i]) % TABLE_SIZE
+template <typename T>
+int HashTable<T>::getKeyFromString(const string value, const int steps) const
+{
+    int x1, x2;
+    if (value.length() == 1) // single character
+    {
+        x1 = (int) value[0];
+        x2 = (int) value[0] + 1;
+    }
+    else // multi character
+    {
+        x1 = (int) value[0] + (int) value[1];
+        x2 = (int) value[0] * (int) value[1];
+    }
+
+    return ((x1 % 53) + ((37 - (x2 % 37)) * steps)) % TABLE_SIZE;
+}
+
+template <typename T>
+int HashTable<T>::getKeyFromInt(const int value, const int steps) const
+{
+    int x1, x2;
+    string convertedValue = to_string(value);
+    if (convertedValue.length() == 1) // single digit
+    {
+        x1 = value;
+        x2 = value * value;
+    }
+    else // multi digit
+    {
+        x1 = (int) convertedValue[0] + (int) convertedValue[1];
+        x2 = (int) convertedValue[0] * (int) convertedValue[1];
+    }
+
+    return ((x1 % 53) + ((37 - (x2 % 37)) * steps)) % TABLE_SIZE;
+}
+
+template <typename T>
+void HashTable<T>::display() const
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != nullptr)
+        {
+            const T* objectPtr = table[i];
+            cout << "Index: " << i << ", Contents: ";
+            objectPtr->print();
+            objectPtr = nullptr;
+        }
+    }
+}
+
+template <typename T>
+HashTable<T>::~HashTable()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        delete table[i];
+    }
+}
 
 #endif
