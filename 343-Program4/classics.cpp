e1f4eddfdd684fcad39a5a445b48b8825c7694d5
EthanPhonsouk <ethan.phonsouk@hotmail.com>
2025-03-15 23:56:02 -0700

    some comments

--- 343-Program4/classics.cpp
+++ 343-Program4/classics.cpp
@@ -1,5 +1,23 @@
+// ------------------------------------------------ classics.cpp------------------------------------------------------- 
+
+// Tristan Santor, Ethan Phonsouk  CSS 343
+
+// 3/14/25
+
+// 3/14/25
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Purpose - implementation of the classics type: child class of movie
+
+// -------------------------------------------------------------------------------------------------------------------- 
 #include "classics.h"
 
+// ---------------------------------------- Classics Constructor ----------------------------------------
+// Description: Initializes a Classics movie object with month, year, and main actor details.
+// preconditions: Valid stock, director, title, month, year, and actor names provided.
+// postconditions: Classics object initialized with all provided values and identifiers.
+// --------------------------------------------------------------------------------------------
 Classics::Classics(int movStock, string movDirector, string movTitle, int movReleaseMon, int movReleaseYr, string firstName, string lastName) 
         :  Movie(movStock, movDirector, movTitle, movReleaseYr), releaseMonth(movReleaseMon), MAFirstName(firstName), MALastName(lastName)
 {
@@ -8,31 +26,61 @@ Classics::Classics(int movStock, string movDirector, string movTitle, int movRel
     code = "C";     // C for classics
 }
 
+// ---------------------------------------- getItemType ----------------------------------------
+// Description: Returns the itemType identifier ("D").
+// preconditions: None.
+// postconditions: Returns "D" for DVD.
+// --------------------------------------------------------------------------------------------
 string Classics::getItemType() const
 {
     return itemType;
 }
 
+// ---------------------------------------- getDVDType ----------------------------------------
+// Description: Returns the DVDType identifier ("M").
+// preconditions: None.
+// postconditions: Returns "M" for Movie.
+// --------------------------------------------------------------------------------------------
 string Classics::getDVDType() const
 {
     return DVDType;
 }
 
+// ---------------------------------------- getCode ----------------------------------------
+// Description: Returns the code for Classics movies ("C").
+// preconditions: None.
+// postconditions: Returns "C" for Classics genre.
+// --------------------------------------------------------------------------------------------
 string Classics::getCode() const
 {
     return code;
 }
 
+// ---------------------------------------- getTitle ----------------------------------------
+// Description: Returns the title of the Classics movie.
+// preconditions: Title must be initialized.
+// postconditions: Returns the movie title.
+// --------------------------------------------------------------------------------------------
 string Classics::getTitle() const
 {
     return title;
 }
 
+// ---------------------------------------- getDirector ----------------------------------------
+// Description: Returns the director of the Classics movie.
+// preconditions: Director must be initialized.
+// postconditions: Returns the movie director.
+// --------------------------------------------------------------------------------------------
 string Classics::getDirector() const
 {
     return director;
 }
 
+// ---------------------------------------- getStock ----------------------------------------
+// Description: Returns the current stock of the Classics movie.
+// preconditions: Stock must be initialized.
+// postconditions: Returns stock as an integer.
+// --------------------------------------------------------------------------------------------
 int Classics::getStock() const {
     return stock;   
 }
@@ -42,6 +90,11 @@ string Classics::getMA() const
     return MAFirstName + " " + MALastName;
 }
 
+// ---------------------------------------- adjustStock ----------------------------------------
+// Description: Adjusts stock up or down based on increment flag, bounded by limits.
+// preconditions: Stock >= 0; increment is true (increase) or false (decrease).
+// postconditions: Returns true if stock adjusted; false if at bounds.
+// --------------------------------------------------------------------------------------------
 bool Classics::adjustStock(const bool increment)
 {
     if (increment && stock < stockCap)
@@ -57,6 +110,11 @@ bool Classics::adjustStock(const bool increment)
     return false; // cannot decrease stock below 0
 }
 
+// ---------------------------------------- isSimilar ----------------------------------------
+// Description: Checks if two Classics movies are similar except for actor names.
+// preconditions: classicsPtr must not be null.
+// postconditions: Returns true if month, year, director, and title match but actor names differ.
+// --------------------------------------------------------------------------------------------
 bool Classics::isSimilar(Classics* classicsPtr) const
 {
     //perform null check
@@ -70,9 +128,20 @@ bool Classics::isSimilar(Classics* classicsPtr) const
         (MAFirstName.compare(classicsPtr->MAFirstName) != 0 || MALastName.compare(classicsPtr->MALastName) != 0));
 }
 
+// ---------------------------------------- formatSortCriteria ----------------------------------------
+// Description: Returns a formatted string used for sorting Classics movies.
+// preconditions: Valid releaseMonth, releaseYear, and actor names.
+// postconditions: Returns "month year FirstName LastName" as a string.
+// --------------------------------------------------------------------------------------------
 string Classics::formatSortCriteria() const {
     return to_string(releaseMonth) + " " + to_string(releaseYear) + " " + MAFirstName + " " + MALastName;
 }
+
+// ---------------------------------------- operator< ----------------------------------------
+// Description: Less-than comparison for Classics by year, month, first then last name.
+// preconditions: The passed Movie must be a Classics type.
+// postconditions: Returns true if this Classics is less than the other.
+// --------------------------------------------------------------------------------------------
 bool Classics::operator<(const Movie& movie) const {
     const Classics& classics = dynamic_cast<const Classics&>(movie); // Now a reference
 
@@ -88,6 +157,11 @@ bool Classics::operator<(const Movie& movie) const {
     return MALastName.compare(classics.MALastName) < 0;
 }
 
+// ---------------------------------------- operator> ----------------------------------------
+// Description: Greater-than comparison for Classics by year, month, first then last name.
+// preconditions: The passed Movie must be a Classics type.
+// postconditions: Returns true if this Classics is greater than the other.
+// --------------------------------------------------------------------------------------------
 bool Classics::operator>(const Movie& movie) const {
     const Classics& classics = dynamic_cast<const Classics&>(movie); // Now a reference
 
@@ -103,7 +177,11 @@ bool Classics::operator>(const Movie& movie) const {
     return MALastName.compare(classics.MALastName) > 0;
 }
 
-
+// ---------------------------------------- operator== ----------------------------------------
+// Description: Equality comparison for Classics based on month, year, and actor names.
+// preconditions: The passed Movie must be a Classics type.
+// postconditions: Returns true if all fields match.
+// --------------------------------------------------------------------------------------------
 bool Classics::operator==(const Movie& movie) const {
     const Classics& classics = dynamic_cast<const Classics&>(movie); // Now a reference
 
@@ -113,6 +191,12 @@ bool Classics::operator==(const Movie& movie) const {
             MALastName.compare(classics.MALastName) == 0);
             
 }
+
+// ---------------------------------------- print ----------------------------------------
+// Description: Outputs the Classics movie's details in a formatted line.
+// preconditions: All fields must be initialized.
+// postconditions: Displays the movie's information.
+// --------------------------------------------------------------------------------------------
 void Classics::print() const
 {
     cout << setw(8) << left << itemType << setw(7) << left << code << setw(35) << left << title << setw(23) << left <<
